a0e22451292e5c9da27904ecaf1ca4f00bd2bfcb
ilham1127 <ilhamipradini@gmail.com>
2023-06-09 00:01:34 +0700

    home screen

--- read.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn new mysqli("localhost", "root", "","note_todolist");
-$query mysqli_query($conn, "select * from note_todolist");
-$data mysqli_fetch_all($query, MYSQLI_ASSOC);
-echo json_encode($data);
-
-?>
